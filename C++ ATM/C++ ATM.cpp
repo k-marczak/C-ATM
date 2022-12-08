8132a2af7cd4ec07d7a350b37fe0fa71ae9aa65f
k-marczak <k_marczak@outlook.com>
2022-12-08 14:36:14 +0200

    Create the menu

--- C++ ATM/C++ ATM.cpp
+++ C++ ATM/C++ ATM.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+
+
+void showMenu() {
+
+    std::cout << "***** MENU *****" << std::endl;
+    std::cout << "1). Check balance" << std::endl;
+    std::cout << "2). Deposit" << std::endl;
+    std::cout << "3). Withdraw" << std::endl;
+    std::cout << "****************" << std::endl;
+    
+}
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
