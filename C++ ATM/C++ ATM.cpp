487522a5a2d35dec9b88a3aec500eebd5361c4cf
k-marczak <k_marczak@outlook.com>
2022-12-08 15:05:52 +0200

    Created the functionality of the program

--- C++ ATM/C++ ATM.cpp
+++ C++ ATM/C++ ATM.cpp
@@ -8,6 +8,7 @@ void showMenu() {
     std::cout << "1). Check balance" << std::endl;
     std::cout << "2). Deposit" << std::endl;
     std::cout << "3). Withdraw" << std::endl;
+    std::cout << "4). Exit" << std::endl;
     std::cout << "****************" << std::endl;
     
 }
@@ -16,7 +17,37 @@ void showMenu() {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    int balance = 500;
+    int deposit;
+    int option;
+    int withdraw;
+
+    do{
+
+    showMenu();
+    std::cout << "Choose the option: ";
+    std::cin >> option;
+
+        if (option == 1) {
+            std::cout << "Your balance is: " << balance << std::endl;
+        }
+        else if (option == 2) {
+            std::cout << "How much money you want to deposit: ";
+            std::cin >> deposit;
+            balance += deposit;
+            std::cout << "Your balance now is: " << balance << std::endl;
+        }
+        else if (option == 3) {
+            std::cout << "How much money you want to withdraw: ";
+            std::cin >> withdraw;
+            balance -= withdraw;
+            std::cout << "Your balance now is: " << balance << std::endl;
+        }
+    } while (option != 4);
+
+
+    return 0;
 }
 
 
